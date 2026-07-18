aa404f1164721b4707283ea7150e9a242dc90cff
himanshusehrawat123456-hub <himanshusehrawat123456@gmail.com>
2026-07-18 18:20:40 +0530

    Implement task management features in todo.py
    Added functionality to manage tasks including adding, showing, and removing tasks.

--- todo.py
+++ todo.py
@@ -7,3 +7,59 @@ def todo():
         file.write(task + "\n")
 
     print("Task saved successfully.")
+# DG AI Todo System
+
+tasks = []
+
+def add_task():
+    task = input("Enter your task: ")
+    tasks.append(task)
+    print("Task added successfully!")
+
+
+def show_tasks():
+    if len(tasks) == 0:
+        print("No tasks available")
+    else:
+        print("\nYour Tasks:")
+        for i, task in enumerate(tasks, 1):
+            print(i, task)
+
+
+def remove_task():
+    show_tasks()
+    
+    number = int(input("Enter task number to remove: "))
+
+    if number <= len(tasks):
+        removed = tasks.pop(number - 1)
+        print(removed, "removed")
+    else:
+        print("Invalid task number")
+
+
+while True:
+
+    print("\n--- DG AI Todo Menu ---")
+    print("1. Add Task")
+    print("2. Show Tasks")
+    print("3. Remove Task")
+    print("4. Exit")
+
+    choice = input("Choose option: ")
+
+    if choice == "1":
+        add_task()
+
+    elif choice == "2":
+        show_tasks()
+
+    elif choice == "3":
+        remove_task()
+
+    elif choice == "4":
+        print("DG AI Todo Closed")
+        break
+
+    else:
+        print("Invalid choice")
