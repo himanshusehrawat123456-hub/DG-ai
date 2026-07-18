38e9786e371c2d852f5aeafe22314f4a9854f62c
himanshusehrawat123456-hub <himanshusehrawat123456@gmail.com>
2026-07-18 19:20:07 +0530

    Implement user login and main menu for DG AI
    Refactor DG AI main file to include user login and menu options.

--- Main.py
+++ Main.py
@@ -1,15 +1,63 @@
-from chat import chat
-from calculator import calculator
+# DG AI Main File
 
-print("===== DG AI =====")
-print("1. Chat")
-print("2. Calculator")
+import login
+import calculator
+import todo
+import chat
+import command
+import storage
+import memory
+import notes
 
-choice = input("Choose: ")
 
-if choice == "1":
-    chat()
-elif choice == "2":
-    calculator()
-else:
-    print("Invalid Choice")
+def main():
+
+    user = login.login()
+
+    if user:
+
+        print("\nWelcome To DG AI")
+
+        while True:
+
+            print("\n===== DG AI MENU =====")
+            print("1. Calculator")
+            print("2. Todo")
+            print("3. Chat")
+            print("4. Command")
+            print("5. Memory")
+            print("6. Notes")
+            print("7. Exit")
+
+            choice = input("Enter choice: ")
+
+            if choice == "1":
+                calculator.calculator()
+
+            elif choice == "2":
+                todo.todo()
+
+            elif choice == "3":
+                chat.chat()
+
+            elif choice == "4":
+                command.command()
+
+            elif choice == "5":
+                memory.memory()
+
+            elif choice == "6":
+                notes.notes()
+
+            elif choice == "7":
+                print("DG AI Closed")
+                break
+
+            else:
+                print("Invalid Choice")
+
+    else:
+        print("Login Failed")
+
+
+main()
